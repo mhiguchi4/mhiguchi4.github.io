f6915f7bbd01cafd7be44a031ef9ec0ad222d9cb
mhiguchi4 <mhiguchi4@gmail.com>
2015-10-26 22:34:34 -0500

    Javascript and responsiveness

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
-//when user clicks on "filter by neighborhood"
-//drop down menu appears
+
+
+
+//when user clicks on one of the neighborhoods in #filter,
+//photos will filter based on that neighborhood selected
+$("#filter").on("change", function() {
+	// Find out what the user selected and save to a variable
+	var selectedLocation = $("#filter").val();
+
+	// if selectedLocation = default
+	if (selectedLocation === "default") {
+		// Fade in all thumbnails
+		$(".thumbnail").fadeIn(350);
+	// else
+	} else {
+		$(".thumbnail").fadeOut(350, function () {
+			// Show all thumbnails with the corresponding class
+			$('.f-' + selectedLocation).fadeIn(350);
+		});
+	}
+});
